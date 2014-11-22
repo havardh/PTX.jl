5bccfc428fbed81c9782f95c152abe5e985f9360
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2014-11-22 16:01:18 +0100

    Added support for multiple types in gpu.jl

--- gpu.jl
+++ gpu.jl
@@ -19,13 +19,13 @@ type GPUArray{T}
   data::Array{T,1}
 end
 
-@noinline getindex{T}(A::GPUArray{T}, i::Int64) = 0
-@noinline setindex!{T}(A::GPUArray{T}, x, i) = A[i] = x
+@noinline getindex{T}(A::GPUArray{T}, i::Int64) = A.data[i]
+@noinline setindex!{T}(A::GPUArray{T}, x::T, i::Int64) = A.data[i] = x
 
 llvm_type(::Type{Int32}) = "i32"
 llvm_type(::Type{Int64}) = "i64"
-llvm_type(::Type{Float32}) = "f32"
-llvm_type(::Type{Float64}) = "f64"
+llvm_type(::Type{Float32}) = "float"
+llvm_type(::Type{Float64}) = "double"
 llvm_type{T}(::Type{GPUArray{T}}) = llvm_type(T) * "*"
 
 function code_ptx(code)
