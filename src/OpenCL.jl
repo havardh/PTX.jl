6f3bf2351fa64f4393b9339e96520b8b0ff05e5a
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2014-12-16 12:58:42 +0100

    Added barrier to OpenCL.jl

--- src/OpenCL.jl
+++ src/OpenCL.jl
@@ -1,2 +1,9 @@
 @inline get_global_id(id::Int64) = get_global_id(int32(id))
 @noinline get_global_id(id::Int32) = int64(id)
+
+@noinline barrier(::Int32) = nothing
+
+
+export
+  get_global_id,
+  barrier
