e8dde1a8d990b0f93a68eabe356d9e664bbcd7d2
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2014-12-16 12:57:49 +0100

    Updated benchmark kernels

--- benchmark/mm/kernel/Float32.cu
+++ benchmark/mm/kernel/Float32.cu
@@ -1,19 +1,23 @@
 extern "C"
 {
-
-  __global__ void MatrixMultiply(
-    const float *A, const float *B, float *C,
-    int n, int m, int k) {
-
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
-    int col = blockIdx.y * blockDim.y + threadIdx.y;
-
+  
+  __device__ float computeCell(const float *A, const float *B, float *C, int row, int col, long n) {
+    
     float v=0;
-    for (int i=0; i<m; i++) {
-      v += A[i + row*m] * B[col + i*k];
+    if (row < n && col < n) {
+      for (long i=0; i<n; i++) {
+	v += A[i + row*n] * B[col + i*n];
+      }
     }
-
-    C[col + row*k] = v;
+    return v;
+  }
+  
+  __global__ void MatrixMultiply(const float *A, const float *B, float *C, long n) {
+    
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    
+    C[col + row*n] = computeCell(A, B, C, row, col, n);
   }
   
 }
